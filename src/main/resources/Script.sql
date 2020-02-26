7a8bdc5d94acf4540a1896564f9d5e26add41341
admin <admin@192.168.1.229>
2020-02-26 17:14:52 +0530

    chanages....comminting

--- src/main/resources/Script.sql
+++ src/main/resources/Script.sql
@@ -6,3 +6,16 @@ CREATE TABLE Vehicles (
  loaded_wt INT,
  max_load INT,
  PRIMARY KEY (vehicle_no));
+
+ 
+ 
+ shankar Script
+ DROP TABLE IF EXISTS trucks_list;
+CREATE TABLE trucks_list (
+ id INT,
+ Truck_No VARCHAR(40) NOT NULL,
+ Truck_Name VARCHAR(40) NOT NULL,
+ Truck_Company VARCHAR(40) NOT NULL,
+ Truck_Owner VARCHAR(40) NOT NULL,
+ DriverMoNo VARCHAR(40) NOT NULL,
+ PRIMARY KEY (id));
